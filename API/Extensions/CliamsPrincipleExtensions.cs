902c7c5feca55f8e5d9a61a49512562603685d87
KiranMR <60286317+KiranMR96@users.noreply.github.com>
2021-06-01 14:37:08 +0530

    End of section 11

---
+++ API/Extensions/CliamsPrincipleExtensions.cs
@@ -0,0 +1,14 @@
+using System.Security.Claims;
+
+namespace API.Extensions
+{
+    public static class ClaimsPrincipleExtensions
+    {
+        public static string GetUsername(this ClaimsPrincipal user)
+        {
+            // var claimsIdentity = User.Identity as System.Security.Claims.ClaimsIdentity;
+            // var username = claimsIdentity.Name;
+            return user.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        }
+    }
+}
--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -20,8 +20,8 @@ export class AccountService {
       map((response: User) => {
         const user = response;
         if (user) {
-          localStorage.setItem('user', JSON.stringify(user));
-          this.currentUserSource.next(user);
+          // this.currentUserSource.next(user);
+          this.setCurrentUser(user);
         }
         return user;
       })
@@ -32,8 +32,9 @@ export class AccountService {
     return this.http.post(this.baseurl + 'account/register', model).pipe(
       map((user: User) => {
         if (user) {
-          localStorage.setItem('user', JSON.stringify(user));
-          this.currentUserSource.next(user);
+          // localStorage.setItem('user', JSON.stringify(user));
+          // this.currentUserSource.next(user);
+          this.setCurrentUser(user);
         }
         return user;
       })
@@ -41,6 +42,7 @@ export class AccountService {
   }
 
   setCurrentUser(user: User) {
+    localStorage.setItem('user', JSON.stringify(user));
     this.currentUserSource.next(user);
   }
 
